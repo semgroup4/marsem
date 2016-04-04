aeeccc2d5fc1855ab211e3ede5c17f7e46df095b
Philip Crockett <philipmayoo@gmail.com>
2016-04-04 10:53:14 +0200

    Created the bare tests for the lap around an object

---
+++ server/tests/lap.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestLapFeature(unittest.TestCase):
+
+    def test_step_count(self):
+        self.assertEqual(3,3)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
